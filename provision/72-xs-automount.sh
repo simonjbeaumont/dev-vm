3fea67237e674a5b20b51323073960c53d0548da
Si Beaumont <simon.beaumont@citrix.com>
2015-10-16 13:10:03 +0100

    automount: Only mount yp:auto.groups map
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- provision/72-xs-automount.sh
+++ provision/72-xs-automount.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 apt-get install -yq autofs5
 
-echo "automount: nis" >> /etc/nsswitch.conf
+#echo "automount: nis" >> /etc/nsswitch.conf
 
-echo "yp:auto.groups --timeout=3600" >> /etc/auto.master
-echo "yp:auto.misc --timeout=3600" >> /etc/auto.master
-echo "yp:auto.nfs --timeout=3600" >> /etc/auto.master
+echo "/usr/groups yp:auto.groups --timeout=3600" >> /etc/auto.master
 
-mv /etc/auto.misc /etc/auto.misc.old
-
+sed -i '/^BROWSE_MODE/ s/no/yes/' /etc/default/autofs
 echo "daemonoptions='tcp,udp,rsize=32768,wsize=32768'" >> /etc/default/autofs
 
 service autofs restart
