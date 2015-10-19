e66b8bb6c01dbef891869c6476192ca9a3f572e0
Si Beaumont <simon.beaumont@citrix.com>
2015-10-19 08:25:44 +0100

    automount: Mount NIS home directories on /nis
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- provision/72-xs-automount.sh
+++ provision/72-xs-automount.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 apt-get install -yq autofs5
 
-#echo "automount: nis" >> /etc/nsswitch.conf
-
 echo "/usr/groups yp:auto.groups --timeout=3600" >> /etc/auto.master
+echo "/nis        yp:auto.home   --timeout=3600" >> /etc/auto.master
 
 sed -i '/^BROWSE_MODE/ s/no/yes/' /etc/default/autofs
 echo "daemonoptions='tcp,udp,rsize=32768,wsize=32768'" >> /etc/default/autofs
