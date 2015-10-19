54775c370251d6b82e599ac765dba7b09111de87
Si Beaumont <simon.beaumont@citrix.com>
2015-10-19 08:26:33 +0100

    user: No need to use /local/home now NIS is out of the way
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- provision/70-xs-user.sh
+++ provision/70-xs-user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 groupadd -g 10020 xendev
-useradd -u 2279 -g 10020 -s /bin/bash -b /local/home -m simonbe
+useradd -u 2279 -g 10020 -s /bin/bash -m simonbe
 
 usermod -aG docker simonbe
 # usermod -aG mock simonbe
